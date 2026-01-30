80a748229fd4222ae6004301392b9ace79d12f37
MOHAMED ASHIQ <mohamedashiq782@gmail.com>
2026-01-30 20:48:19 +0530

    Inline cn function in select component to avoid path issues

--- frontend/components/ui/select.tsx
+++ frontend/components/ui/select.tsx
@@ -3,8 +3,12 @@
 import * as React from "react"
 import * as SelectPrimitive from "@radix-ui/react-select"
 import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from "lucide-react"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-import { cn } from "../../lib/utils"
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 function Select({
   ...props
